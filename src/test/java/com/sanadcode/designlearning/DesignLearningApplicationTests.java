6b463260213c148e8bec566cd1de9571a3360164
Moataz Sanad <sanad.code.io@gmail.com>
2024-04-25 06:48:06 +0200

    init

---
+++ src/test/java/com/sanadcode/designlearning/DesignLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanadcode.designlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
